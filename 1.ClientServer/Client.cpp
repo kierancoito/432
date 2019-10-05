28e2c83d18cb0661f1185b27a00c8882865fcb71
kierancoito <keirancoito@gmail.com>
2019-10-05 11:41:04 -0700

    added includes

--- 1.ClientServer/Client.cpp
+++ 1.ClientServer/Client.cpp
@@ -1,4 +1,10 @@
-//
 // Created by Kieran Coito on 2019-10-05.
-//
-
+#include <sys/types.h>    // socket, bind
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <netinet/in.h>   // htonl, htons, inet_ntoa
+#include <arpa/inet.h>    // inet_ntoa
+#include <netdb.h>        // gethostbyname
+#include <unistd.h>       // read, write, close
+#include <strings.h>      // bzero
+#include <netinet/tcp.h>  // SO_REUSEADDR
+#include <sys/uio.h>      // writev
