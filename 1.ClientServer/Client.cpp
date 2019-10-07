c63ded0402774df17a8d2b8aa6848d4f4bca0139
kierancoito <keirancoito@gmail.com>
2019-10-07 12:55:30 -0700

    Added variables to specify what each parameter for both classes correlates to. Added error messages if parameters are not correct.

--- 1.ClientServer/Client.cpp
+++ 1.ClientServer/Client.cpp
@@ -8,10 +8,38 @@
 #include <strings.h>      // bzero
 #include <netinet/tcp.h>  // SO_REUSEADDR
 #include <sys/uio.h>      // writev
+#include <iostream>
 
 using namespace std;
 
 int main(int argumentNum, char *argument[]){
+    /*
+     * port: server's port number
+     * repetition: the number of iterations a client performs on data transmission using "single write," "writev" or "multiple writes."
+     * nbufs: the number of data buffers
+     * bufsize: the size of each data buffer (in bytes)
+     * serverIp: server's IP address
+     * type: the type of transfer scenario: 1, 2, or 3 (see below)
+     */
+
+    if(argumentNum != 6){
+        cout << "Incorrect number of arguments, please try again with the proper number, which is 6" << endl;
+    }
+
+    int port = (int) *argument[0];
+    int repetition = (int) *argument[1];
+    int nbufs = (int) *argument[2];
+    int bufsize = (int) *argument[3];
+    int serverIp = (int) *argument[4];
+    int type = (int) *argument[5];
+
+    if(nbufs * bufsize != 1500){
+        cout << "Either nbufs or bufsize are the wrong size. nbufs * bufize must equal 1500" << endl;
+    }
+    char databuf[nbufs][bufsize];
+
+    //2050 for port
+    int clientSocker = socket(AF_INET ,SOCK_STREAM, 0);
 
 
 }
--- 1.ClientServer/Server.cpp
+++ 1.ClientServer/Server.cpp
@@ -8,10 +8,25 @@
 #include <strings.h>      // bzero
 #include <netinet/tcp.h>  // SO_REUSEADDR
 #include <sys/uio.h>      // writev
+#include <iostream>
 
 using namespace std;
 
 int main(int argumentNum, char *argument[]){
+    /*
+     * port: server's port number
+     * repetition: the repetition of client's data transmission activities. This value should be the same as the "repetition" value used by the client. It means the number of iterations a server shall perform on "read" from the socket. If a client performs "repetition of 100" data transmissions, then the server shall perform at least "repetition of 100" reads from the socket to completely receive all data sent by the client.
+     * Therefore, the value "repetition" shall be the same for both client and server to use.
+     *
+     */
+    if(argumentNum != 2){
+        cout << "Incorrect number of arguments, please try again with the proper number, which is 6" << endl;
+    }
+
+    int port = (int) *argument[0];
+    int repetition = (int) *argument[1];
+
+    int serverSocket = socket(AF_INET ,SOCK_STREAM, 0);
 
 
 }
