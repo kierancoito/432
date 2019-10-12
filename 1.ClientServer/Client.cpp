80971ba226edac0110705a2f4b672a1f7ee4de3d
kierancoito <keirancoito@gmail.com>
2019-10-11 20:54:43 -0700

    Everything

--- 1.ClientServer/Client.cpp
+++ 1.ClientServer/Client.cpp
@@ -1,45 +1,155 @@
-// Created by Kieran Coito on 2019-10-05.
 #include <sys/types.h>    // socket, bind
 #include <sys/socket.h>   // socket, bind, listen, inet_ntoa
 #include <netinet/in.h>   // htonl, htons, inet_ntoa
 #include <arpa/inet.h>    // inet_ntoa
 #include <netdb.h>        // gethostbyname
 #include <unistd.h>       // read, write, close
-#include <strings.h>      // bzero
+#include <string.h>       // bzero
 #include <netinet/tcp.h>  // SO_REUSEADDR
+
+#include <stdlib.h>       // EXIT_FAILURE
 #include <sys/uio.h>      // writev
+#include <sys/time.h>     // timeval
+#include <string>
 #include <iostream>
+#include <exception>
 
 using namespace std;
 
-int main(int argumentNum, char *argument[]){
-    /*
-     * port: server's port number
-     * repetition: the number of iterations a client performs on data transmission using "single write," "writev" or "multiple writes."
-     * nbufs: the number of data buffers
-     * bufsize: the size of each data buffer (in bytes)
-     * serverIp: server's IP address
-     * type: the type of transfer scenario: 1, 2, or 3 (see below)
-     */
 
-    if(argumentNum != 6){
-        cout << "Incorrect number of arguments, please try again with the proper number, which is 6" << endl;
+
+int MULTIWRITE = 1;
+int WRITEV = 2;
+int SINGLE_WRITE = 3;
+
+int port, repetition, nbufs, bufsize, type;
+char *serverIp;
+
+/*
+ * Takes in the user arguments and verifies that they are all valid
+ */
+bool verifyArgs(int argumentNum, char *argument[]) {
+
+    if (argumentNum != 7) {
+        cout << "Need 7 args. Instead got " << argumentNum << " args." << endl;
+        return false;
     }
 
-    int port = (int) *argument[0];
-    int repetition = (int) *argument[1];
-    int nbufs = (int) *argument[2];
-    int bufsize = (int) *argument[3];
-    int serverIp = (int) *argument[4];
-    int type = (int) *argument[5];
+    try {
+        port = stoi(argument[1]);
+        repetition = stoi(argument[2]);
+        nbufs = stoi(argument[3]);
+        bufsize = stoi(argument[4]);
+        serverIp = argument[5];
+        type = stoi(argument[6]);
 
-    if(nbufs * bufsize != 1500){
-        cout << "Either nbufs or bufsize are the wrong size. nbufs * bufize must equal 1500" << endl;
+    } catch (invalid_argument ) {
+        cout << "One of your arguments was incorrect please check over and try again" << endl;
+        return false;
+    } catch (exception ) {
+        cout << "One of your arguments was incorrect please check over and try again" << endl;
+        return false;
     }
+
+    //ensure valid port
+    if (port > 49152 || port < 1024) {
+        cout << "Port number invalid: " << port << endl;
+        return false;
+    }
+
+    //ensure total buffer doesn't exceed 1500
+    if (nbufs * bufsize != 1500) {
+        cout << "Number of buffers * buffer size != 1500" << endl;
+        cout << "Number of buffers: " << nbufs << ", ";
+        cout << "Buffer size: " << bufsize << "." << endl;
+        return false;
+    }
+    //ensure valid number of repitions
+    if (repetition < 0) {
+        cout << "Number of times repeated is negative!" << endl;
+        return false;
+    }
+    //ensure tpe is correct
+    if (type != 1 && type != 2 && type != 3) {
+        cout << " Type must be 1, 2, or 3. You entered " << type  << endl;
+        return false;
+    }
+    return true;
+}
+
+void results(struct timeval start, struct timeval lap, struct timeval end, int numberOfRead) {
+    // Second to microsecond needs to multiply by 1000000
+    long dataSendingTime = (lap.tv_sec - start.tv_sec) * 1000000 + lap.tv_usec - start.tv_usec;
+    long roundTripTime = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
+    cout << "data-sending time = " + to_string(dataSendingTime) + " usec, ";
+    cout << "round-trip time = " + to_string(roundTripTime) + " usec, ";
+    cout << "Number of reads =  " + to_string(numberOfRead) << endl;
+    return;
+}
+
+int main(int argumentNum, char *argument[]) {
+
+    if (!verifyArgs(argumentNum, argument)) {
+        cout << "Arguments were invalid." << endl;
+        return -1;
+    }
+
     char databuf[nbufs][bufsize];
 
-    //2050 for port
-    int clientSocker = socket(AF_INET ,SOCK_STREAM, 0);
+    // set up data structure and connect to port
+    // as specified in lecture slides
+    struct hostent* host = gethostbyname(serverIp);
+    sockaddr_in sendSockAddr;
+    bzero( (char *)&sendSockAddr, sizeof(sendSockAddr));
+    sendSockAddr.sin_family = AF_INET; // Address Family Internet
+    sendSockAddr.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
+    sendSockAddr.sin_port = htons(port);
 
+    int clientSD = socket(AF_INET, SOCK_STREAM, 0); //SOCK_STREAM = TCP, 0 = IP
+    int connectStatus = connect( clientSD, (sockaddr*)&sendSockAddr, sizeof(sendSockAddr));
+    if (connectStatus < 0) {
+        cerr << "Connection failed!" << endl;
+        close(clientSD);
+        return -1;
+    }
+
+    struct timeval start, lap, end;
+    //start timer
+    gettimeofday(&start, NULL);
+    for (int i = 0; i <= repetition; i++) {
+
+        //different write scenarios as specified by assignment specifications
+        if (type == MULTIWRITE) {
+            for (int j = 0; j < nbufs; j++) {
+                write(clientSD, databuf[j], bufsize);    // sd: socket descriptor
+            }
+        }
+        if (type == WRITEV) {
+            struct iovec vector[nbufs];
+            for (int j = 0; j < nbufs; j++) {
+                vector[j].iov_base = databuf[j];
+                vector[j].iov_len = bufsize;
+            }
+            writev(clientSD, vector, nbufs);           // sd: socket descriptor
+        }
+        if (type == SINGLE_WRITE) {
+            write(clientSD, databuf,nbufs * bufsize);
+        }
+    }
+    //get time after sending happened
+    gettimeofday(&lap, nullptr);
+
+    int numberOfRead;
+
+    //read back from server to confirm writing was finished
+    read(clientSD, &numberOfRead, sizeof(numberOfRead));
+
+    //get end time
+    gettimeofday(&end, nullptr);
+
+    //print results of test
+    results(start, lap, end, numberOfRead);
+    close(clientSD);
+    return 0;
 
 }
--- 1.ClientServer/Server.cpp
+++ 1.ClientServer/Server.cpp
@@ -1,4 +1,3 @@
-// Created by Kieran Coito on 2019-10-05.
 #include <sys/types.h>    // socket, bind
 #include <sys/socket.h>   // socket, bind, listen, inet_ntoa
 #include <netinet/in.h>   // htonl, htons, inet_ntoa
@@ -9,24 +8,137 @@
 #include <netinet/tcp.h>  // SO_REUSEADDR
 #include <sys/uio.h>      // writev
 #include <iostream>
+#include <sys/time.h>
+#include <pthread.h>
 
 using namespace std;
 
-int main(int argumentNum, char *argument[]){
-    /*
-     * port: server's port number
-     * repetition: the repetition of client's data transmission activities. This value should be the same as the "repetition" value used by the client. It means the number of iterations a server shall perform on "read" from the socket. If a client performs "repetition of 100" data transmissions, then the server shall perform at least "repetition of 100" reads from the socket to completely receive all data sent by the client.
-     * Therefore, the value "repetition" shall be the same for both client and server to use.
-     *
-     */
-    if(argumentNum != 2){
-        cout << "Incorrect number of arguments, please try again with the proper number, which is 6" << endl;
+int port, repetition, serverSD, newSD;
+const int bufSize = 1500;
+const int connection = 5;
+int uniCount = 0;
+
+//struct to hold thread data for pthread
+struct thread_data{
+    int sd;
+    int repetition;
+};
+
+/*
+ * Verify all user arguments are valid
+ */
+bool verifyArgs(int argumentNum, char *argument[]){
+    if (argumentNum != 3){
+        cerr << "Invalid number of arguments. " << argumentNum << " was how many you entered. This requires 3" << endl;
+        return false;
+    }
+    try{
+        port = stoi(argument[1]);
+        repetition = stoi(argument[2]);
+    } catch(invalid_argument& iaException){
+        cerr << iaException.what() << endl;
+        return false;
+    } catch(exception e){
+        cerr << e.what() << endl;
+        return false;
+    }
+    if (port < 1024 || port > 49152){
+        cerr << "Bad port number" << endl;
+        return false;
+    }
+    if (repetition < 0){
+        cerr << "bad reptition" << endl;
+        return false;
+    }
+    return true;
+
+}
+
+/*
+ * Method used to execute the different type scenarios in a p thread
+ *
+ * Parameter
+ *      threadData - the data about socket connection and repetitions
+ */
+void * thread_server(void *threadData){
+    //increment count everytime this function is called
+    //while the instance of the server is running
+    uniCount++;
+    //create data buffer
+    char databuf[bufSize];
+    //extract data about connection and repetition
+    struct thread_data *data;
+    data = (struct thread_data *) threadData;
+    struct timeval start;
+    struct timeval stop;
+    int count = 0;
+
+    //get intial time
+    gettimeofday(&start , nullptr);
+
+    //go through all data
+    for ( int i = 0; i <= repetition; i++ ){
+        for ( int numberRead = 0; (numberRead += read(data->sd , databuf , bufSize - numberRead)) < bufSize; ++count );
+        count++;
     }
 
-    int port = (int) *argument[0];
-    int repetition = (int) *argument[1];
+    //get final time
+    gettimeofday(&stop , nullptr);
 
-    int serverSocket = socket(AF_INET ,SOCK_STREAM, 0);
+    //find time difference
+    long totalTime = (stop.tv_sec - start.tv_sec) * 1000000 + (stop.tv_usec - start.tv_usec);
 
+    //write back to client
+    write(data->sd , &count , sizeof(count));
 
+    //display results to user
+    cout << "Data-receiving time for thread " << uniCount <<  " = " + to_string(totalTime) + " usec" << endl;
+
+    // finish with this client. close it
+    close(data->sd);
+}
+
+/*
+ * Main function that will handle the majority of the server application work
+ */
+int main(int argumentNum, char *argument[]){
+    //verify all input from the user is valid
+    if (!verifyArgs(argumentNum, argument)){
+        cout << "Arguments were invalid." << endl;
+        return -1;
+    }
+
+    //create data structures for socket connection as specified in lecture slides
+    sockaddr_in acceptSocketAddr;
+    bzero((char *)& acceptSocketAddr, sizeof(acceptSocketAddr));
+    acceptSocketAddr.sin_family = AF_INET;
+    acceptSocketAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    acceptSocketAddr.sin_port = htons(port);
+
+    // Open socket and bind
+    serverSD = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM = TCP, 0 = IP
+    const int on = 1;
+    setsockopt(serverSD, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(int));
+    bind(serverSD, (sockaddr*)&acceptSocketAddr, sizeof(acceptSocketAddr));
+
+    // Listen and accept
+    listen(serverSD, connection);
+    sockaddr_in newSockAddr;
+    socklen_t newSockAddrSize = sizeof(newSockAddr);
+
+    //run forever
+    while(true){
+        int newSd = accept(serverSD, (sockaddr *)&newSockAddr, &newSockAddrSize);
+
+        //create new thread and run thread_server on that thread with the information received from the client
+        pthread_t new_thread;
+        struct thread_data *data;
+
+        //save data into thread_data object from socket that will be used in thread_server
+        data = new thread_data();
+        data->repetition = repetition;
+        data->sd = newSd;
+
+        int iret1 = pthread_create( &new_thread, NULL, thread_server, (void*) data );
+    }
 }
