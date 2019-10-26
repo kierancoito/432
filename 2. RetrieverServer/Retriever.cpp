c0770187b900f9c994207f1f275433ed45f6f981
kierancoito <keirancoito@gmail.com>
2019-10-25 18:01:25 -0700

    Lots of changes mostly works.

--- 2. RetrieverServer/Retriever.cpp
+++ 2. RetrieverServer/Retriever.cpp
@@ -1,192 +1,218 @@
-#include <sys/types.h>    // socket, bind
-#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
-#include <netinet/in.h>   // htonl, htons, inet_ntoa
-#include <arpa/inet.h>    // inet_ntoa
-#include <netdb.h>        // gethostbyname
-#include <unistd.h>       // read, write, close
-#include <string.h>       // bzero
-#include <netinet/tcp.h>  // SO_REUSEADDR
-#include <stdlib.h>       // EXIT_FAILURE
-#include <sys/uio.h>      // writev
-#include <sys/time.h>     // timeval
-#include <string>
-#include <iostream>
-#include <exception>
-#include <fstream>
-
-using namespace std;
-
-/**
- * Created by Kieran Coito
- * CSS 432
- * October 11th 2019
- *
- *
- */
-
-int port;
-char *serverName, *fileName;
-
-string NAME = "savedPage.txt";
-
-/**
- *
- * @param argumentNum
- * @param argument
- * @return
- */
-bool verifyArgs(int argumentNum, char *argument[]) {
-
-    if (argumentNum != 4) {
-        cout << "Need 4 args. Instead got " << argumentNum << " args." << endl;
-        return false;
-    }
-
-    try {
-        port = stoi(argument[1]);
-        serverName = argument[2];
-        fileName = argument[3];
-
-    } catch ( exception ) {
-        cout << "One of your arguments was incorrect please check over and try again" << endl;
-        return false;
-    }
-
-    //ensure valid port
-    if (port != 80 && (port > 49152 || port < 1024) {
-        cout << "Port number invalid: " << port << endl;
-        return false;
-    }
-
-    //fix server name if needed
-    if(serverName.substr(0,6) == "http://"){
-        serverName = serverName.substr(7, serverName.length-7);
-
-    }
-    if(serverName.substr(0,7) == "https://"){
-        serverName.substr(8 ,serverName.length-7);
-
-    }
-    return true;
-}
-
-/**
- *
- * @param sd
- * @return
- */
-string processGet(int sd){
-
-    string header = "";
-    char last = 0;
-
-    while ( true ){
-
-        char current = 0;
-        recv(sd , &current , 1 , 0);
-        // For each header, it is ended with a \r\n
-        if ( current == '\n' || current == '\r' ){
-            if ( last == '\r' && current == '\n' ){
-                break;
-            }
-
-        }else{
-            header += current;
-        }
-        last = current;
-    }
-    return header;
-}
-
-/**
- *
- * @param SD
- */
-void receiveHtml(int SD){
-
-    int length = 0;
-
-    while ( true ){
-        string response = processGet(SD);
-
-        //end of header
-        if ( response == "" ){
-            break;
-        }
-
-        cout << response << endl;
-
-        //number of bytes that will be in the body of the message
-        if ( response.substr(0 , 15) == "Content-Length:" ){
-            length = atoi(response.substr(16 , response.length()).c_str());
-        }
-    }
-
-    //open file to save website
-    ofstream output;
-    output.open(NAME);
-
-    //create buffer size of website file and receive that website file into the buffer
-    char buffer[length];
-    recv(SD , &buffer , length , 0);
-
-    //convert char array to a single string
-    string html = "";
-    for ( int i = 0; i < length; i++ ){
-        html += buffer[i];
-    }
-
-    //save html to file and display it to the user
-    output << html;
-    cout << html << endl;
-
-    //close file and socket
-    close(SD);
-    output.close();
-    return;
-}
-
-/**
- *
- * @param argumentNum nunmber of arguments
- * @param argument servername, filename, and portnumber as received as command line arguments
- * @return
- */
-int main(int argumentNum, char *argument[]) {
-
-    if (!verifyArgs(argumentNum, argument)) {
-        cout << "Arguments were invalid." << endl;
-        return -1;
-    }
-
-    // set up data structure and connect to port
-    // as specified in lecture slides
-    struct hostent* host = gethostbyname(serverName);
-    sockaddr_in sendSockAddr;
-    bzero( (char *)&sendSockAddr, sizeof(sendSockAddr));
-
-    sendSockAddr.sin_family = AF_INET;
-    sendSockAddr.sin_addr.s_addr = inet_addr( inet_ntoa(*(struct in_addr*)*host->h_addr_list) );
-    sendSockAddr.sin_port = htons(port);
-
-    int clientSD = socket(AF_INET, SOCK_STREAM, 0);
-
-    int connectStatus = connect( clientSD, (sockaddr*)&sendSockAddr, sizeof(sendSockAddr));
-    if (connectStatus < 0) {
-        cerr << "Connection failed!" << endl;
-        close(clientSD);
-        return -1;
-    }
-    //create get request
-    string getRequest = string("GET " + string(fileName) + " HTTP/1.1\r\n"
-                               + "Host: " + string(serverName) + "\r\n" + "\r\n");
-    //send get request to host
-    int result = send(clientSD, getRequest.c_str(), sizeof(getRequest.c_str()), 0);
-    if ( result <= 0 ){
-        cout << "Unable to send the request";
-        return -1;
-    }
-    //receive back from server
-    receiveHtml(connectStatus);
-    return 0;
-}
+//#include <sys/types.h>    // socket, bind
+//#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+//#include <netinet/in.h>   // htonl, htons, inet_ntoa
+//#include <arpa/inet.h>    // inet_ntoa
+//#include <netdb.h>        // gethostbyname
+//#include <unistd.h>       // read, write, close
+//#include <string.h>       // bzero
+//#include <netinet/tcp.h>  // SO_REUSEADDR
+//#include <stdlib.h>       // EXIT_FAILURE
+//#include <sys/uio.h>      // writev
+//#include <sys/time.h>     // timeval
+//#include <string>
+//#include <iostream>
+//#include <exception>
+//#include <fstream>
+//
+//using namespace std;
+//
+///**
+// * Created by Kieran Coito
+// * CSS 432
+// * October 11th 2019
+// *
+// *
+// */
+//
+//int port;
+//char *serverName, *fileName;
+//
+//string NAME = "savedPage.txt";
+//
+///**
+// *
+// * @param argumentNum
+// * @param argument
+// * @return
+// */
+//bool verifyArgs(int argumentNum, char *argument[]) {
+//
+//    if (argumentNum != 4) {
+//        cout << "Need 4 args. Instead got " << argumentNum << " args." << endl;
+//        return false;
+//    }
+//
+//    char * tempServer;
+//    try {
+//        port = stoi(argument[3]);
+//        tempServer = argument[1];
+//        fileName = argument[2];
+//
+//    } catch ( exception ) {
+//        cout << "One of your arguments was incorrect please check over and try again" << endl;
+//        return false;
+//    }
+//
+//    //ensure valid port
+//    if (port != 80 && (port > 49152 || port < 1024)) {
+//        cout << "Port number invalid: " << port << endl;
+//        return false;
+//    }
+//
+//    //fix server name if needed
+//    string server = tempServer;
+//    if(server.substr(0,7).compare("http://") == 0){
+//        server = server.substr(7, server.length()-7);
+//
+//    }
+//    if(server.substr(0,8).compare("https://") == 0){
+//        server = server.substr(8 ,server.length()-7);
+//
+//    }
+//
+//    serverName = new char[server.length()];
+//    strcpy(serverName, server.c_str());
+//    return true;
+//}
+//
+///**
+// *
+// * @param sd
+// * @return
+// */
+//string processGet(int sd){
+//
+//    string header = "";
+//    char last = 0;
+//
+//    while ( true ){
+//
+//        char current = 0;
+//        recv(sd , &current , 1 , 0);
+//        // For each header, it is ended with a \r\n
+//        if ( current == '\n' || current == '\r' ){
+//            if ( last == '\r' && current == '\n' ){
+//                break;
+//            }
+//
+//        }else{
+//            header += current;
+//        }
+//        last = current;
+//    }
+//    return header;
+//}
+//
+///**
+// *
+// * @param SD
+// */
+//void receiveHtml(int SD){
+//
+//    int length = 0;
+//    int statusCode;
+//    bool stillInGet = true;
+//
+//    //pull each line of the get request and parse it for needed information
+//    //stillInGet will act as flag to stay in loop
+//    while ( stillInGet == true ){
+//
+//        //get current line of the get request
+//        string responseLine = processGet(SD);
+//
+//        //get the length of the file being request from the get response
+//        if ( responseLine.substr(0 , 15).compare("Content-Length:") == 0 ){
+//            length = atoi(responseLine.substr(16 , responseLine.length()).c_str());
+//        }
+//
+//        //get the status code of the page from the get response
+//        if(responseLine.substr(0, 8).compare("HTTP/1.1 ") == 0){
+//            statusCode = stoi(responseLine.substr(9, 3));
+//        }
+//        //end of header so set flag to false to exit loop
+//        if ( responseLine == "" ){
+//            stillInGet = false;
+//        }
+//    }
+//
+//    if(length == 0){
+//        cout << "This request returned no body to display or save" << endl;
+//        close(SD);
+//    }
+//    //open file to save website
+//    ofstream output;
+//
+//    //get body of file being request
+//    char buffer[length];
+//    recv(SD , &buffer , length , 0);
+//
+//    //convert char array to a single string
+//    string html = "";
+//    for ( int i = 0; i < length; i++ ){
+//        html += buffer[i];
+//    }
+//
+//    //save html to file and display it to the user if status code is 200
+//    cout << html << endl;
+//    if(statusCode == 200){
+//        output.open(NAME);
+//        output << html;
+//
+//    }
+//
+//    //close file and socket
+//    close(SD);
+//    if(statusCode == 200){
+//        output.close();
+//    }
+//    return;
+//}
+//
+///**
+// *
+// * @param argumentNum nunmber of arguments
+// * @param argument servername, filename, and portnumber as received as command line arguments
+// * @return
+// */
+//int main(int argumentNum, char *argument[]) {
+//
+//
+//    //argumentNum = 4; //
+//    //char * tempArg[] = {"", "http://google.com", "/index.html", "80"};
+//    //argument = tempArg;
+//    if (!verifyArgs(argumentNum, argument)) {
+//        cout << "Arguments were invalid." << endl;
+//        return -1;
+//    }
+//
+//    // set up data structure and connect to port
+//    // as specified in lecture slides
+//    struct hostent* host = gethostbyname(serverName);
+//    sockaddr_in sendSockAddr;
+//    bzero( (char *)&sendSockAddr, sizeof(sendSockAddr));
+//
+//    sendSockAddr.sin_family = AF_INET;
+//    sendSockAddr.sin_addr.s_addr = inet_addr( inet_ntoa(*(struct in_addr*)*host->h_addr_list) );
+//    sendSockAddr.sin_port = htons(port);
+//
+//    int clientSD = socket(AF_INET, SOCK_STREAM, 0);
+//
+//    int connectStatus = connect( clientSD, (sockaddr*)&sendSockAddr, sizeof(sendSockAddr));
+//    if (connectStatus < 0) {
+//        cerr << "Connection failed!" << endl;
+//        close(clientSD);
+//        return -1;
+//    }
+//    //create get request
+//    string getRequest = "GET " + string(fileName) + " HTTP/1.1\r\n"
+//                               + "Host: " + string(serverName) + "\r\n" + "\r\n";
+//    //send get request to host
+//    int result = send(clientSD, getRequest.c_str(), strlen(getRequest.c_str()), 0);
+//    if ( result <= 0 ){
+//        cout << "Unable to send the request";
+//        return -1;
+//    }
+//    //receive back from server
+//    receiveHtml(clientSD);
+//    return 0;
+//}
--- 2. RetrieverServer/Server.cpp
+++ 2. RetrieverServer/Server.cpp
@@ -11,21 +11,19 @@
 #include <sys/time.h>
 #include <pthread.h>
 #include <string.h>
+
 using namespace std;
 /**
  * Created by Kieran Coito
  * CSS 432
  *
  */
-
-
-
 int port, serverSD, newSD;
 const int connection = 5;
 int uniCount = 0;
 
 //struct to hold thread data for pthread
-struct thread_data{
+struct thread_data {
     int sd;
 };
 
@@ -35,19 +33,19 @@ struct thread_data{
  * @param argument
  * @return
  */
-bool verifyArgs(int argumentNum, char *argument[]){
-    if (argumentNum != 2){
+bool verifyArgs(int argumentNum, char *argument[]) {
+    if (argumentNum != 2) {
         cerr << "Invalid number of arguments. " << argumentNum << " was how many you entered. This requires 3" << endl;
         return false;
     }
-    try{
+    try {
         port = stoi(argument[1]);
 
-    } catch ( exception ) {
+    } catch (exception) {
         cout << "One of your arguments was incorrect please check over and try again" << endl;
         return false;
     }
-    if (port != 80 && (port < 1024 || port > 49152)){
+    if (port != 80 && (port < 1024 || port > 49152)) {
         cerr << "Bad port number" << endl;
         return false;
     }
@@ -77,7 +75,6 @@ string processGet(int sd){
 
         }else{
             header += current;
-
         }
         last = current;
     }
@@ -86,24 +83,154 @@ string processGet(int sd){
 
 /**
  *
+ * @param statusCode
+ * @return
+ */
+string statusResponse(int statusCode){
+    string response = "";
+    switch(statusCode){
+        case 200:
+            response = "HTTP/1.1 200 OK\r\n";
+            break;
+        case 400:
+            response = "HTTP/1.1 400 Bad Request\r\n";
+            break;
+        case 401:
+            response = "HTTP/1.1 401 Unauthorized\r\n";
+            break;
+        case 403:
+            response = "HTTP/1.1 403 Forbidden\r\n";
+            break;
+        case 404:
+            response = "HTTP/1.1 404 Not Found\r\n";
+            break;
+        default:
+            break;
+    }
+    return response;
+}
+
+/**
+ *
+ * @param filePath
+ * @param statusCode
+ * @param fileContent
+ */
+void response(string &filePath, string &statusCode, string &fileContent) {
+    fileContent = "";
+
+    // Trying to access a file that is above the directory the server is running
+    if (filePath.substr(0, 2).compare("..") == 0) {
+        fileContent = "Accessing that file is Forbidden";
+        statusCode = statusResponse(403);
+
+    }else if (filePath.length() >= 15) {
+
+        if (filePath.substr(filePath.length() - 15, filePath.length()).compare("SecretFile.html") == 0) {
+            fileContent = "Access to that file is Unauthorized";
+            statusCode = statusResponse(401);
+        }
+
+    } else {
+        // All the other cases is valid
+        // Need to append a . in front to know start to search from current path
+        filePath = "." + filePath;
+        cout << "Looking for this file " + filePath << endl;
+        FILE *file = fopen(filePath.c_str(), "r");
+
+        // Could not open the file because it doesn't exist
+        if (file == nullptr) {
+
+            fileContent = "That file could not be found";
+            statusCode = statusResponse(404);
+
+        } else {
+            // Found the file
+            while (!feof(file)) {
+
+                string line;
+                char c = fgetc(file);
+                if (c < 0) {
+                    continue;
+                    // Encountered not supported character. Skip that character
+                }
+                // Manually append these to the string response
+                if (c == '\n') {
+                    fileContent += '\n';
+                    continue;
+                } else if (c == '\r') {
+                    fileContent += "\r";
+                    continue;
+                }
+                fileContent += c;
+            }
+            fclose(file);
+            statusCode = statusResponse(200);
+        }
+    }
+}
+
+/**
+ *
  * @param threadData
  * @return
  */
-void * processGet(void *threadData){
+void *processGet(void *threadData) {
     //extract data about connection and repetition
     struct thread_data *data;
     data = (struct thread_data *) threadData;
     //get the message from the socket
-    string getRequest = processGet(data->sd);
+    int sd = data->sd;
+    string file = "";
+    bool getting = false;
 
-    if(getRequest.substr(0,3) == "GET"){
+    while (getting == false) {
+        //read each line in the get request for all needed info
+        string getRequest = processGet(sd);
 
+        cout << "	Header: " << getRequest << "\n";
+
+        //extract file name once it is found which should be after the GET in the first line of the request
+        if (getRequest.substr(0, 3) == "GET") {
+            file = getRequest.substr(4, getRequest.length() - 13);
+
+            //todo remove once done testing
+            cout << "GOT file!  " << file << "\n";
+            getting = true;
+        }
+
+        //this will be hit if the entire request is parsed and a file is not found
+        if (getRequest == "") {
+            break;
+        }
     }
 
+    string statusCode;
+    string fileContent;
+    if (getting == true) {
+        response(file, statusCode, fileContent);
 
+    } else {
+        // Could not recognize the get request
+        fileContent = "Request could not be processed. Please verify formatting";
+        statusCode = statusResponse(400);
+    }
 
-    // finish with this client. close it
-    close(data->sd);
+    string pageLength = to_string(fileContent.size());
+
+    //each line of response message
+    string locationLine = "Location " + file;
+    string contentLine = "Content-Length: " + pageLength + "\r\n";
+    string contentType = "Content-Type: text/plain\r\n\r\n";
+
+    //create full response message
+    string responseFull = statusCode + locationLine + contentLine + contentType + fileContent;
+
+    cout << "Printing out the response " + responseFull << endl;
+
+    send(sd, &responseFull[0], responseFull.size(), 0);
+    close(sd);
+    return 0;
 }
 
 /**
@@ -112,20 +239,20 @@ void * processGet(void *threadData){
  * @param argument
  * @return
  */
-int main(int argumentNum, char *argument[]){
-    //verify all input from the user is valid
+int main(int argumentNum, char *argument[]) {
 
-    //
-    // NEEDS ONE ARG PORT NUM
-    //
-    if (!verifyArgs(argumentNum, argument)){
+    argumentNum = 2; //
+    char * tempArg[] = {"", "2050"};
+    argument = tempArg;
+    //verify all input from the user is valid
+    if (!verifyArgs(argumentNum, argument)) {
         cout << "Arguments were invalid." << endl;
         return -1;
     }
 
     //create data structures for socket connection as specified in lecture slides
     sockaddr_in acceptSocketAddr;
-    bzero((char *)& acceptSocketAddr, sizeof(acceptSocketAddr));
+    bzero((char *) &acceptSocketAddr, sizeof(acceptSocketAddr));
     acceptSocketAddr.sin_family = AF_INET;
     acceptSocketAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     acceptSocketAddr.sin_port = htons(port);
@@ -133,8 +260,8 @@ int main(int argumentNum, char *argument[]){
     // Open socket and bind
     serverSD = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM = TCP, 0 = IP
     const int on = 1;
-    setsockopt(serverSD, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(int));
-    bind(serverSD, (sockaddr*)&acceptSocketAddr, sizeof(acceptSocketAddr));
+    setsockopt(serverSD, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(int));
+    bind(serverSD, (sockaddr *) &acceptSocketAddr, sizeof(acceptSocketAddr));
 
     // Listen and accept
     listen(serverSD, connection);
@@ -142,8 +269,8 @@ int main(int argumentNum, char *argument[]){
     socklen_t newSockAddrSize = sizeof(newSockAddr);
 
     //run forever
-    while(true){
-        newSD = accept(serverSD, (sockaddr *)&newSockAddr, &newSockAddrSize);
+    while (true) {
+        newSD = accept(serverSD, (sockaddr *) &newSockAddr, &newSockAddrSize);
 
         //create new thread and run thread_server on that thread with the information received from the client
         pthread_t new_thread;
@@ -153,6 +280,7 @@ int main(int argumentNum, char *argument[]){
         data = new thread_data();
         data->sd = newSD;
 
-        int iret1 = pthread_create( &new_thread, NULL, processGet, (void*) data );
+        int iret1 = pthread_create(&new_thread, NULL, processGet, (void *) data);
     }
 }
+
