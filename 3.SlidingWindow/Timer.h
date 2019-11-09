c7be25e38182e6c9e4f787c8cf9009fb3013ec33
kierancoito <keirancoito@gmail.com>
2019-11-08 17:32:10 -0800

    updated formatting, started work on stop and wait

--- 3.SlidingWindow/Timer.h
+++ 3.SlidingWindow/Timer.h
@@ -16,16 +16,16 @@ extern "C"
 }
 
 class Timer {
- public:
-  Timer( );                  // Constructor
-  void start( );             // Memorize the curren time in startTime
-  long lap( );               // endTime - startTime
-  long lap( long oldTv_sec, long oldTv_usec ); // endTime - oldTime
-  long getSec( );            // get startTime.tv_sec
-  long getUsec( );           // get startTime.tv_usec
- private:
-  struct timeval startTime;  // Memorize the time to have started an evaluation
-  struct timeval endTime;    // Memorize the time to have stopped an evaluation
+public:
+    Timer();                  // Constructor
+    void start();             // Memorize the curren time in startTime
+    long lap();               // endTime - startTime
+    long lap(long oldTv_sec, long oldTv_usec); // endTime - oldTime
+    long getSec();            // get startTime.tv_sec
+    long getUsec();           // get startTime.tv_usec
+private:
+    struct timeval startTime;  // Memorize the time to have started an evaluation
+    struct timeval endTime;    // Memorize the time to have stopped an evaluation
 };
 
 #endif
--- 3.SlidingWindow/udphw3.cpp
+++ 3.SlidingWindow/udphw3.cpp
@@ -5,6 +5,10 @@
 
 // functions for stop and wait implementation
 
+// Stop-and-wait test implements the "stop-and-wait" algorithm, i.e., a client writes a message sequence number in
+// message[0], sends message[] and waits until it receives an integer acknowledgment of that sequence number from a
+// server, while the server receives message[], copies the sequence number from message[0] to an acknowledgment
+
 /**
  *
  * @param sock
@@ -13,6 +17,24 @@
  * @return
  */
 int clientStopWait( UdpSocket &sock, const int max, int message[] ){
+    // sends message[] and receives an acknowledgment from the server max (=20,000) times using the sock object. If the
+    // client cannot receive an acknowledgment immediately, it should start a Timer. If a timeout occurs (i.e., no
+    // response after 1500 usec), the client must resend the same message. The function must count the number of
+    // messages retransmitted and return it to the main function as its return value.
+
+    Timer* timeout = new Timer();
+    int retransmitNum = 0;
+
+    for(int i = 0; i < max; i++){
+
+        //send message
+
+
+
+
+
+    }
+
 
 }
 
@@ -23,10 +45,20 @@ int clientStopWait( UdpSocket &sock, const int max, int message[] ){
  * @param message
  */
 void serverReliable( UdpSocket &sock, const int max, int message[] ){
+    // receiving message[] and sending an acknowledgment at a server side max (=20,000) times using the sock object.
 
 }
 
-//functions for sliding window implementation
+// functions for sliding window implementation
+//
+// Sliding window implements the "sliding window" algorithm, using the first element in the message[] for storing
+// sequence numbers (in messages sent by client) and acknowledgement numbers (in messages sent by server).
+//
+// A client keeps writing a message sequence number in message[0] and sending message[] as long as the number of
+// in-transit messages is less than a given windowSize.
+//
+// The server receives message[], saves the message's sequence number (stored in message[0]) in a local buffer array
+// and returns a cumulative acknowledgement, i.e., the last received message in order.
 
 /**
  *
@@ -37,7 +69,15 @@ void serverReliable( UdpSocket &sock, const int max, int message[] ){
  * @return
  */
 int clientSlidingWindow( UdpSocket &sock, const int max, int message[], int windowSize ){
-
+    // sends message[] and receiving an acknowledgment from a server max (=20,000) times using the sock object. As
+    // described above, the client can continuously send a new message[] and increasing the sequence number as long as
+    // the number of in-transit messages (i.e., # of unacknowledged messages) is less than "windowSize." That number
+    // should be decremented every time the client receives an acknowledgment. If the number of unacknowledged messages
+    // reaches "windowSize," the client should start a Timer. If a timeout occurs (i.e., no response after 1500 usec),
+    // it must resend the message with the minimum sequence number among those which have not yet been acknowledged.
+    // The function must count the number of messages (not bytes) re-transmitted and return it to the main function as
+    // its return value.
+    // NOTE: the windowSize in this assignment refers to the number of messages, not the number of bytes
 }
 
 /**
@@ -48,5 +88,8 @@ int clientSlidingWindow( UdpSocket &sock, const int max, int message[], int wind
  * @param windowSize
  */
 void serverEarlyRetrans( UdpSocket &sock, const int max, int message[], int windowSize ){
+    // receives message[] and sends an acknowledgment to the client max (=20,000) times using the sock object. Every
+    // time the server receives a new message[], it must save the message's sequence number in an array and return a
+    // cumulative acknowledgment, i.e., the last received message in order.
 
 }
